26ea1247f89252d39d59bb31c610c4a76dcaa608
nosolopau <pau@nosolopau.com>
2015-01-25 12:58:46 +0100

    Basic views

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_redbooth request.env['omniauth.auth']
 
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      @user.update_from_omniauth(request.env["omniauth.auth"]) unless @user.new_record?
+
+      sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: 'Redbooth') if is_navigational_format?
     else
       session['devise.redbooth_data'] = request.env['omniauth.auth']
--- app/models/user.rb
+++ app/models/user.rb
@@ -26,12 +26,20 @@ class User
   field :uid, type: String
   field :provider, type: String
   field :name, type: String
+  field :credentials, type: Hash
+  field :extra, type: Hash
 
   def self.from_redbooth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.name = auth.info.name
       user.password = Devise.friendly_token[0,20]
+      user.extra = auth.extra
+      user.credentials = auth.credentials
     end
+  end
+
+  def update_from_omniauth(auth)
+    update_attributes(email: auth.info.email, credentials: auth.credentials, extra: auth.extra)
   end
 end
