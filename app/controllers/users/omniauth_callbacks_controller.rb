911f9b9463ea82716d9a9db764b1ef9bcd2dca72
nosolopau <pau@nosolopau.com>
2015-01-29 19:48:26 +0100

    Final views

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user.update_from_omniauth(request.env['omniauth.auth']) unless @user.new_record?
 
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Redbooth') if is_navigational_format?
     else
       session['devise.redbooth_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,11 @@ module ApplicationHelper
     '<span class="glyphicon glyphicon-calendar red-logo" aria-hidden="true"></span>'.html_safe
   end
 
-  def linked_twitter_account?
-    user_signed_in? && current_user.linked_twitter_account?
+  def ask_for_link_twitter_account?
+    user_signed_in? && !current_user.linked_twitter_account?
+  end
+
+  def bootstrap_class_for(flash_type)
+    {success: 'alert-success', error: 'alert-error', alert: 'alert-warning', notice: 'alert-info'}[flash_type.to_sym] || flash_type.to_s
   end
 end
