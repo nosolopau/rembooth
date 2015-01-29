7848f1293d5f729c344f9d64dd4c8f1edce92ac9
nosolopau <pau@nosolopau.com>
2015-01-29 20:03:07 +0100

    Added jobs type to schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,6 +22,9 @@
 env :MAILTO, 'pau@nosolopau.com'
 env :PATH, ENV['PATH']
 
+job_type :unicorn_start, 'cd :path && bundle exec unicorn -c :path/config/unicorn.rb -E :environment -D'
+job_type :rake, 'cd :path && RAILS_ENV=:environment bundle exec rake :task'
+
 every 15.minutes do
   rake 'reminder_notifier:run'
 end
